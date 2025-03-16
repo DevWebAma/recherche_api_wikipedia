6608b84984437a6fd77b4cb6a07b1d8ddf1606e3
DevWebAma <148153050+DevWebAma@users.noreply.github.com>
2025-03-16 19:56:22 +0100

    write (fetch)

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 // API ENDPOINT : `https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srlimit=20&srsearch=${searchInput}`
+
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const errorMsg = document.querySelector(".error-msg");
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(e) {
+  e.preventDefault();
+
+  if (input.value === " ") {
+    errorMsg.textContent = "Wops, veuillez remplir l'input";
+    return;
+  } else {
+    errorMsg.textContent = "";
+    wikiApiCall(input.value);
+  }
+}
+
+async function wikiApiCall(searchInput) {
+  const response = await fetch(
+    `https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srlimit=20&srsearch=${searchInput}`
+  );
+
+  const data = await response.json();
+  console.log(data);
+}
